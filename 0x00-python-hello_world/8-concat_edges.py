6191213e7d26012ab70c388e2a84f0f4b98aa3b5
chiagoziem11 <chiagoziem11@yahoo.com>
2023-01-03 06:39:54 -0800

    string concatination

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-panguage that combines remarkable power with very clear syntax"
+    language that combines remarkable power with very clear syntax"
 str = str[39:66] + str[-33:-17] + str[:6]
 print(str)
