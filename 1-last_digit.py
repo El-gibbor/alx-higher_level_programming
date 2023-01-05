b4e52a4a1362be5e2df94dee40686b138320b2f4
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-01-05 08:27:25 +0400

    MESSAGE

---
+++ 1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+value = number % 10
+if value > 5:
+    print(f"Last digit of {number:d} is {value:d} and is greater than 5")
+elif value == 0:
+    print(f"Last digit of {number:d} is {value:d} and is 0")
+elif value < 6:
+    print(f"Last digit of {number:d} is {value:d} and less than 6 and not 0")
