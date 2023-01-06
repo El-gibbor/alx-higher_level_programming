86842199a56c2c4c00cf25bddce1adefcc2a3ae6
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-01-06 10:58:23 +0400

    print ascii alphabet values

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+int_str = str(number)
+lastValue = int(int_str[-1])
+if number < 0 and lastValue != 0:
+    print("Last digit of {} is -{} and is less than 6 and not 0")
 
-
-def lastValue():
-    int_str = str(number)
-    lastValue = int(int_str[-1])
-    return -lastValue if (number < 0) else lastValue
-
-
-print(f"Last digit of {number:d} is {lastValue():d} and is", end=' ')
-if lastValue() > 5:
-    print("greater than 5")
-elif lastValue() > 6 and lastValue() != 0:
-    print("less than 6 and not 0")
-elif lastValue() == 0:
-    print("0")
-elif lastValue() < 0 and lastValue() != 0:
-    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for alpha in range(97, 122):
-    print(f"{chr(alpha)}", end='') #convert integer to character using chr function
+    print("{}".format(chr(alpha)), end='') #convert integer to character using chr function
