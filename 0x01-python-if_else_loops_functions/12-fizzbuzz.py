43a7066082677aae4de1eb1882cf32019fd20b7e
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-01-09 11:48:21 +0400

    fizz, buzz & fizbuzz for multiples of 3, 5 and 15

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 100):
-        if i % 3 == 0:
+        if i % 15 == 0:
+            print("Fizzbuzz", end=' ')
+        elif i % 3 == 0:
             print("Fizz", end=' ')
         elif i % 5 == 0:
             print("Buzz", end=' ')
-        elif i % 15 == 0:
-            print("Fizzbuzz", end=' ')
         else:
-            print(i, end='')
+            print(i, end=' ')
