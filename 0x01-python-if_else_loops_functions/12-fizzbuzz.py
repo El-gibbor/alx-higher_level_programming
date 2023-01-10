cdf0fc07cc76100cc0012a560f2cc0a378890b8d
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-01-10 12:19:02 +0400

    fizzbuzz for multiple of 3 5, then, both 3 and 5

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 15 == 0:
-            print("Fizzbuzz", end=' ')
+            print("FizzBuzz", end=' ')
         elif i % 3 == 0:
             print("Fizz", end=' ')
         elif i % 5 == 0:
