96ce901ac65ad301f445448a0ce354accf2187ca
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-01-13 08:26:18 +0400

    function that does the same as a byte code

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-<<<<<<< HEAD
-    return (98 + a**b)
-=======
-    return (98 +  a ** b)
->>>>>>> 6e5b88fabf457acfb74ecf5a2ffd1d3b4316a344
+    return (98 + a ** b)
