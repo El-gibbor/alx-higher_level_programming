c4401973d9dac602487c98fac57d05f5d9d5be8f
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-01-16 15:48:43 +0400

    a function that returns a tuple with the length of a string and its first character.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     """concartinate 2 tuple elements (0 0) to the given tuples"""
-    first_tup = tuple_a + (0, 0)
+    first_tup = tuple_a + (0, 0)  tuple_a(0, 0)
     sec_tup = tuple_b + (0, 0)
     """get the index sum of both tuples"""
     added_tuples = first_tup[0] + sec_tup[0], first_tup[1] + sec_tup[1]
