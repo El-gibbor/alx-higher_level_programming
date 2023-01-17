11c122798ccaad5aecfdc365b4c0af4b2201e975
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-01-17 19:42:14 +0400

    Update 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -2,5 +2,5 @@
 def number_keys(a_dictionary):
     """extract/get all the keys in the dic"""
     num_keys = a_dictionary.keys()
-    """using len() function to return total number of keys"""
+    """used len() function to return total the number of keys"""
     return(len(num_keys))
