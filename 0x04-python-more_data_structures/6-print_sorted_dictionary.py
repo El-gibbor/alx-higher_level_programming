025fef72e5210550cb28eb6a9769332a9608243e
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-01-17 20:26:21 +0400

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    # iterate throu the keys and values (k for keys, v for the values)
+    # iterate through the keys and its value (k for keys, v for the values)
     for k, v in sorted(a_dictionary.items()):
-        # print both keys and values in sorted order
+        # print both keys and values/items in sorted order
         print("{}: {}".format(k, v))
