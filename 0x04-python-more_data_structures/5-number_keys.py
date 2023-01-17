a3fc6004018c1ac44395deda95fa2d8308aee143
chiagoziem11 <chiagoziem11@yahoo.com>
2023-01-17 07:47:07 -0800

    def uniq_add(my_list=[]):

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 def number_keys(a_dictionary):
     """extract/get all the keys in the dic"""
-    num_keys = a_dictionary.keys()
+#    num_keys = a_dictionary.keys()
     """using len() function to return total number of keys"""
-    return(len(num_keys))
+    return(len(a_dictionary))
