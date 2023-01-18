40eea966c73207a6a7dbd4908aa96880e637c80d
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-01-18 09:33:48 +0400

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
+    # using list comprehension, iterate through dict $ mul the values in keys
     return {k: v * 2 for k, v in a_dictionary.items()}
