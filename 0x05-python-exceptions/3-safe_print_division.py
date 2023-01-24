9a93b92c3d9e7583b4b8a61d7c19939577712f00
chiagoziem11 <chiagoziem11@yahoo.com>
2023-01-24 07:10:21 -0800

    a function that divides 2 integers and prints the result.

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,10 @@
 def safe_print_division(a, b):
     try:
         division = a / b
+        # handles error & return None if valued is beign divided by 0
     except ZeroDivisionError:
         division = None
     finally:
+        # finally outputs all (error value -> None & the divisible values
         print("Inside result: {}".format(division))
     return (division)
-
