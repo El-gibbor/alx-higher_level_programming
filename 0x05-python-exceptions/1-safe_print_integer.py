3bc5b1afa9dba97bf0f04849c6158b2ae9e7e34e
chiagoziem11 <chiagoziem11@yahoo.com>
2023-01-24 03:20:47 -0800

    a function that prints an integer with "{:d}".format()

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value), end="")
+        print()
         return True
-    """handles the error of str type parsed to int format"""
+        """handles the error of str type parsed to int format"""
     except ValueError:
         return False
