f208f132fa390d2856c89d2828bdf850a55bea07
chiagoziem11 <chiagoziem11@yahoo.com>
2023-01-26 23:10:10 -0800

    a class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""defining a class, square."""
+
+
+class Square:
+
+    def __init__(self, size=0):
+        """initialising the class, square.
+        Args:
+
+            size(int): size of the square
+
+        raises:
+            TypeError: if size is not of type int
+            ValueError: if size is less than 0
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
