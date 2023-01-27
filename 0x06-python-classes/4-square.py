f76957fc2b31f877ebd72a34d0a179783f755799
chiagoziem11 <chiagoziem11@yahoo.com>
2023-01-27 07:29:54 -0800

    a class Square that defines a square with pivate attribute using setter and hetter metthosd

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""difine a class, square"""
+
+
+class Square:
+    """class with private attribute, size"""
+    
+    def __init__(self, size=0):
+        """initializing the class, square with it's attributes
+
+        Args:
+            size (int): size of the square
+            
+        raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        self.size = size
+        
+    @property
+    def size(self): 
+        """retrieve the size of square"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """sets the value of the attribute, size.
+
+        Args:
+            value (int): new size of the square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        
+    def area(self):
+        """returns the current square area"""
+        return (self.__size ** 2)
