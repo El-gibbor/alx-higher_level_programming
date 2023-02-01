ac9814ba0af2ba0c0c142857b465ed47b79d8c01
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-02-01 13:29:07 +0400

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,29 +11,29 @@ class Rectangle:
         self.height = height
 
     @property
-    def width(self, value):
+    def width(self):
         """retrives the private attribute value"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise TypeError("width must be >= 0")
         return self.__value
 
     @width.setter
     def width(self, value):
         """checks, validate and set/modify the attribute value"""
+         if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise TypeError("width must be >= 0")
         self.__value = value
 
     @property
-    def height(self, VALUE):
+    def height(self, value):
         """reetrieves the private attribute value"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
         return self.__height
 
     @height.setter
     def height(self, value):
         """check, validate and set the value"""
+         if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
         self.__value = value
