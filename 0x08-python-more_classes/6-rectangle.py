e7d92855190d91f3aab4954e6e9a64f129c59b13
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-02-02 23:50:51 +0400

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,6 @@
 
 class Rectangle:
     """defines a rectangle
-
     Args:
         width(int): width of the rectangle
         height(int): height of the rectangle
@@ -24,8 +23,8 @@ class Rectangle:
         """retrieves/return the height attribute"""
         return self.__width
 
-    @height.setter
-    def widthself, value):
+    @width.setter
+    def width(self, value):
         """check, validate and set the value for width attribute"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -46,3 +45,31 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+        
+    def area(self):
+        """returns rectangle area"""
+        return self.__width * self.__height
+    
+    def perimeter(self):
+        """returns the rectangle patimeter"""
+        return (2 * self.__height + self.width)
+    
+    def __str__(self) -> str:
+        """defines string representation of Rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return '\n'.join('#' * self.__width for j in range(self.__height))
+        
+    def __repr__(self):
+        """returns the representation of a class method that can
+        be parsed to create a new instance
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __init__(self, width=0, height=0):
+        type(self).number_of_instances += 1
+        
+    def __del__(self, width=0, height=0):
+        """destructor, called when all refrences of obj are deleted"""
+        print("Bye rectangle...")
+        type(self).number_of_instances -= 1
