09e16b3cb553c1ac40f5fd0b64da56e5baf92aea
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-02-04 13:06:11 +0400

    a class that defines a singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -4,7 +4,7 @@
 
 class Node:
     """defining the node"""
-    
+
     def __init__(self, data, next_node=None):
         """initialising object attributes
 
@@ -14,12 +14,12 @@ class Node:
         """
         self.data = data
         self.next_node = next_node
-        
+
     @property
     def data(self):
         """retrieve/return the data attribute"""
         return self.__data
-    
+
     @data.setter
     def data(self, value):
         """check, validate and set the value for data
@@ -30,12 +30,12 @@ class Node:
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
-        
+
     @property
     def next_node(self):
         """retrieve/return next node attribute"""
         return self.__next_node
-    
+
     @next_node.setter
     def next_node(self, value):
         """check, validate and set next node attribute
@@ -47,27 +47,25 @@ class Node:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
+
 class SinglyLinkedList:
     """defines a singly linked list"""
-    
+
     def __init__(self):
         """initialise an empty singly linked list"""
         self.__head = None
-        
+
     def __str__(self):
         """human-readble reprensation of the singly l-list"""
         node = ""
-        l_node = self.__head # assign an iterator to head to loop through the l-list
+        l_node = self.__head
+        # assign an iterator to head to loop through the l-list
         while l_node:
             # each node data seperated by newline asper alx demand
             node += str(l_node.data) + "\n"
             l_node = l_node.next_node
-        return node[:-1] # ([:-1]) -> exempts a "\n" at the end
-    
-        """using list comprehension (optional)"""
-    # def __str__(self):
-    #       return '\n'.join(str(n.data) for n in self)
-    
+            return node[:-1]  # ([:-1]) -> exempts a "\n" at the end
+
     def sorted_insert(self, value):
         """inserts Nodes in a sorted manner (increasing order)"""
         new_node = Node(value)
@@ -76,7 +74,8 @@ class SinglyLinkedList:
             new_node.next_node = self.__head
             self.__head = new_node
             return
-        while node.next_node is not None and node.next_node.data < new_node.data:
+        while node.next_node is not None and \
+                node.next_node.data < new_node.data:
             node = node.next_node
         new_node.next_node = node.next_node
         node.next_node = new_node
