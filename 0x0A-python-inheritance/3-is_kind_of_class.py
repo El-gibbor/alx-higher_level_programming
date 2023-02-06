f2a03e17f53c58fcb90c5b53992a585e27470797
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-02-06 23:50:47 +0400

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""a func that returns a boolean if an object is an instace of, or the obj is an
-instance of a class that inherited 4rm, the specified class.
+"""a func that returns a boolean if an object is an instace of, or
+the obj is an instance of a class that inherited 4rm, the specified class.
 """
 
 
