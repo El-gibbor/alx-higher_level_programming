4c9a4bdce56dcbd2be0658885c91647331c7a12f
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-02-06 23:24:47 +0400

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" a function that returns the list of available attributes and methods of an object"""
+
+
 class MyList(list):
     """superclass -> list, childclass -> Mylist."""
+    
     def print_sorted(self):
         """prints list in sorted order"""
         print(sorted(self))
