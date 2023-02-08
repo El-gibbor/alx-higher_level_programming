b7c1389de9270f6103a6142e4131a133aab56768
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-02-08 18:32:47 +0400

    a class BaseGeometryc with public method and raises exception error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a class module, baseGeometry"""
+
+
+class BaseGeometry:
+    """defines the class, BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
