8727bac45bd4ceb6adc0299d6f83afe5fc040d3f
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-02-08 18:58:54 +0400

    a class with public method and another method that validates value

--- 0x0A-python-inheritance/7-main.py
+++ 0x0A-python-inheritance/7-main.py
@@ -19,4 +19,4 @@ except Exception as e:
 try:
     bg.integer_validator("distance", -4)
 except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e)
+    print("[{}] {}".format(e.__class__.__name__, e))
