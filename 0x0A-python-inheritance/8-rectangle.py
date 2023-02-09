30195971bd5089364fecfe28becad0b5914a4ce9
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-02-09 14:11:38 +0400

    subclass that inherits from a superclass. area of instance and str reprensation defined

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """using integer_validator() to validate initialised attributes"""
 
-        self.integer_validator("height", height)
+        self.integer_validator(height, height)
         self.__height = height
-        self.integer_validator("width", width)
+        self.integer_validator(width, width)
         self.__width = width
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/pythoin3
+"""imports a module to inherit from"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """defines subclass which inherites from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """using a method from the super class, integer_validator(),
+           to validate subclass initialised attributes
+        """
+        self.integer_validator(height, height)
+        self.__height = height
+        self.integer_validator(height, height)
+        self.__width = width
+
+    def area(self):
+        """returns the area of rectangle"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """prints/return the str representation of discription below"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
