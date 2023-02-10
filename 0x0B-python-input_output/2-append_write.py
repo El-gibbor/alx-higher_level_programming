1fa95e9b892e8c402973688e99b360151d36c08f
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-02-10 20:41:50 +0400

    removed

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,9 +0,0 @@
-"""a module for appending"""
-
-
-def append_write(filename="", text=""):
-    """appends str at the end of a text file and
-        return the number of characters appended
-    """
-    with open(filename, "a", encoding="utf-8") as file:
-        return (file.write(text))
