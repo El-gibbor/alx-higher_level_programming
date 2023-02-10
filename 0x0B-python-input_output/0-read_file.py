dee7471c8ff5b7465e740b4142c2424e380c1a3f
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-02-10 17:21:54 +0400

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """opens the parsed file in default read mode"""
     with open(filename, encoding="utf-8") as file:
-        """prints file content with read method"""
+        """prints file content with read() method"""
         print(file.read(), end="")
