324dbeb2e54e3e57c6469de7c0277085ec515770
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-02-10 16:34:33 +0400

    updated

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -9,8 +9,8 @@ class MyInt(int):
     """
     def __eq__(self, other):
         """== operator is inverted to the opposite of what it does"""
-        return not super().__eq__(other)
+        return self != other
 
     def __ne__(self, other):
         """!= operator is inverted to the opposite of what it does"""
-        return not super().__ne__(other)
+        return self == other
