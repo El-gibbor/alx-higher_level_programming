2db65c2408752d6a60617747c2227c20ac1dc2da
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-02-10 21:03:13 +0400

    a class BaseGeometry (based on 6-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,13 +1,12 @@
-#!/usr/bin/pyhon3
-"""a class module, BaseGeometry"""
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
 
 
-class BaseGeometry:
-    """defines a BaseGeometry"""
+class BaseGeometry():
+    """Represent base geometry."""
 
     def area(self):
-        """raises an error if area is checked"""
-
+        """Not yet implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
@@ -19,7 +18,8 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer". format(name))
-        if value <= 0:
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif 0 >= value:
             raise ValueError("{} must be greater than 0".format(name))
