fafd5092604ea7636f6d425872bd6b503c4a3793
MR-ELGIBBOR <chiagoziem11@yahoo.com>
2023-02-10 21:00:03 +0400

    whitespaces removed

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,8 +4,8 @@
 
 class MyList(list):
     """superclass -> list, childclass -> Mylist."""
-    
+
     def print_sorted(self):
         """prints list in sorted order"""
-        
+
         print(sorted(self))
