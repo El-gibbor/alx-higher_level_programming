f17bd61c26a41df2ff14adbb259bc59486eff36c
El-gibbor <chiagoziem11@yahoo.com>
2023-02-16 21:15:11 +0400

    returns all attributes of the class instance in dic

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,12 +2,15 @@
 """A module with a function that returns the dictionary representation
 of all instance attributes of the class.
 """
+
+
 class Student:
-    
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
+        """returns all attributes of the class instance in dict"""
         return vars(self)
