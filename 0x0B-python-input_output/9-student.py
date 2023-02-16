dd8c5d356de9a7bb6368dbee47d744d0121d43a3
El-gibbor <chiagoziem11@yahoo.com>
2023-02-16 21:12:54 +0400

    returns dict represtion of instance attribuytres

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module with a function that returns the dictionary representation
+of all instance attributes of the class.
+"""
+class Student:
+    
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        return vars(self)
