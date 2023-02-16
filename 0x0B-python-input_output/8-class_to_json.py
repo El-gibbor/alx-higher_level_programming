c4dc2c193099ef2759d556e653a1533f43e732b8
El-gibbor <chiagoziem11@yahoo.com>
2023-02-16 20:22:03 +0400

    a function that returns the dictionary description

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""this module defines a function that return the dictionary description
+or representation of a parsed in instance of a class(obj)
+"""
+
+
 def class_to_json(obj):
     return vars(obj)
