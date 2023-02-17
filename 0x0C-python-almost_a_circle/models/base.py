6a51e08cea3975deb7acf71e726580df816c8d1e
El-gibbor <chiagoziem11@yahoo.com>
2023-02-17 22:32:44 +0400

    base class for all other classes in the project

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a module for the Base of all other classes in this project"""
+
+
+class Base:
+    """defines the base class"""
+    
+    __nb_objects  = 0
+    
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+            
+            
