364fc328478b2bb1714d23cfbbd9b544bbf93f38
El-gibbor <chiagoziem11@yahoo.com>
2023-02-17 22:36:53 +0400

    base class for all classes in this project

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,14 +4,13 @@
 
 class Base:
     """defines the base class"""
-    
-    __nb_objects  = 0
-    
+
+    __nb_objects = 0
+
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-            
             
