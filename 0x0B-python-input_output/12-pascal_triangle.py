e07f832313009a830004cbdd710031cf11b59b70
El-gibbor <chiagoziem11@yahoo.com>
2023-02-20 11:41:19 +0400

    list in a pascal triangle shape. gotten!

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,18 +1,4 @@
-
-#!/usr/bin/python
-# def pascal_triangle(n):
-#     result = []
-#     if n <= 0:
-#         return result
-#     for i in range(n):
-#         row = []
-#         C = 1
-#         for j in range(i+1):
-#             row.append(C)
-#             C = C * (i - j) // (j + 1)
-#         result.append(row)
-#     return result
-
+#!/usr/bin/python3
 def pascal_triangle(n):
     result = []
     if n <= 0:
@@ -26,5 +12,3 @@ def pascal_triangle(n):
         result.append(row)
     for r in result:
         print(r)
-n = 5
-pascal_triangle(n)
