77d8c79afac5c64eff36cdd4b5f9853889d882f4
El-gibbor <chiagoziem11@yahoo.com>
2023-02-22 08:48:44 +0400

    task 8. attribute updates in a given order using arbitrary args (*args)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -117,3 +117,18 @@ class Rectangle(Base):
             for column in range(self.__width):
                 print("#", end="")
             print()
+    
+    def update(self, *args):
+        """updates/assign an argument to each attribute. the else clause
+        ensures the attribute retains its original, previous or default value
+        of optional argument when no argument is provided for update in this method()
+        """
+        self.id = args[0] if len(args) >= 1 else self.id
+        self.width = args[1] if len(args) >= 2 else self.width
+        self.height = args[2] if len(args) >= 3 else self.height
+        self.x = args[3] if len(args) >= 4 else self.x
+        self.y = args[4] if len(args) >= 5 else self.y
+        
+        
+        
+        
