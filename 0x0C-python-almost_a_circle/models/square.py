2bdc2bfad43d436939624b2b7c1758033c4e9afc
El-gibbor <chiagoziem11@yahoo.com>
2023-02-28 10:25:03 +0400

    task 10. square class (iheriting all attr and logic with the super __init__ method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,31 +28,4 @@ class Square(Rectangle):
         self.height = size_val
 
     def __str__(self):
-     return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
-# class Square(Rectangle):
-#     """Represents a square"""
-#     def __init__(self, size, x=0, y=0, id=None):
-#         self.size = size
-#         self.x = x
-#         self.y = y
-#         self.id = None
-#         super().__init__(size, size, x, y, id)
-
-#     def __str__(self):
-#         """Defines a format for the string representation of the class"""
-#         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
-
-#     @property
-#     def size(self):
-#         """Gets the value of size"""
-#         return self.__width
-
-#     @size.setter
-#     def size(self, value):
-#         """Sets the value for size"""
-#         if type(value) is not int:
-#             raise TypeError("width must be an integer")
-#         if value <= 0:
-#             raise ValueError("width must be > 0")
-#         self.__width = value
-#         self.__height = value
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
