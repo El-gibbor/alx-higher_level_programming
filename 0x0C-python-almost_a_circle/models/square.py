f8e529fb2eb63f75cc229fae6dae96e5c33f2836
El-gibbor <chiagoziem11@yahoo.com>
2023-02-28 08:42:11 +0400

    10. square class (inheritence, using the supperclass __init__ logic )

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
 """a module for square which inherites the Rectangle"""
-from rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """defines a Square"""
-    
+
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, width, height)
-    
+        """initializing attr by using the __init__ logic of the superclass"""
+        super().__init__(size, size, x, y, id=None)
+        self.size = size
+
+        """p-decorator for size attribute. this sets the value for both
+        height and width, ensuring the value of height and width are
+        always equal for a square object.
+        """
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size_val):
+        """validations are already implemented in the superclass
+        property decorator & setter
+        """
+        self.width = size_val
+        self.height = size_val
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}"\
+            .format(self.id, self.x, self.y, self.size)
