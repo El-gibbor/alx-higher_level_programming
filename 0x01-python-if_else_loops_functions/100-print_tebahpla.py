e6fa28b6066b5998d055c4cdb42ab43e83783306
El-gibbor <chiagoziem11@yahoo.com>
2023-08-08 18:45:51 +0400

    alphabets in reverse, alternating lower and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    alternated_char = i - 32 if (i % 2) else i
-    print(chr(alternated_char), end='')
+    alternated = i - 32 if (i % 2) else i
+    print('{}'.format(chr(alternated)), end='')
