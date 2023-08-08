b7a13c1df78eb4efc9a611a7a2b802cd6c111614
El-gibbor <chiagoziem11@yahoo.com>
2023-08-08 19:23:44 +0400

    interprete disassembled bytecode to source code

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    for i in range(len(str)):
+
+I DON TIRE! MAKE I FIND FOOD CHOP FIRST, NA PERSON WEY DEY ALIVE DEY DO TASK
--- 0x01-python-if_else_loops_functions/tempCodeRunnerFile.py
+++ 0x01-python-if_else_loops_functions/tempCodeRunnerFile.py
@@ -1,3 +1,11 @@
- # if 97 <= ord(i) <= 122:
-    #         ord(i) + 32
-    #         print(i, end='')
+def remove_char_at(str, n):
+#     for i in range(len(str)):
+#         if i == n:
+#            str[i] == '%'
+#     return str
+    
+# print(remove_char_at("Best School", 3))
+# print(remove_char_at("Chicago", 2))
+# print(remove_char_at("C is fun!", 0))
+# print(remove_char_at("School", 10))
+# print(remove_char_at("Python", -2))
