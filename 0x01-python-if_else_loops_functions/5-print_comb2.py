39a8b729910984b385f5ea77fdab4e3b42dbf11e
El-gibbor <chiagoziem11@yahoo.com>
2023-08-08 13:50:33 +0400

    prints digits from 0 - 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 99:
-        print('{}'.format(str(i).zfill(2)), end=', ')
-    else:
-        print('{}'.format(str(i).zfill(2)))
+    print('{}'.format(str(i).zfill(2)), end=', ' if i < 99 else '\n')
