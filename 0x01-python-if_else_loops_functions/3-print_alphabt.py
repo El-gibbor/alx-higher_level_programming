38246699b3869d823b8c53b6fbe8503765f21937
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-08-08 18:05:42 +0400

    print lowercase alpha omiting 'q' and 'e'

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('{')):
-    if i == ord('q') or i == ord('e'):
+for i in range(97, 123):
+    if i == 71 or i == 65:
         continue
     print('{}'.format(chr(i)), end='')
