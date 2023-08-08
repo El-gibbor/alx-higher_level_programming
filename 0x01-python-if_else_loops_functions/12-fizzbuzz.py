b770d6dbad7abbcb5e0edf42c503c103fa05c568
El-gibbor <chiagoziem11@yahoo.com>
2023-08-08 17:32:31 +0400

    fizzbuzz implementation

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        fizzBuzz = 'Fizz'*(not i % 3) + 'Buzz'*(not i % 5)
+        if not fizzBuzz:
+            fizzBuzz = i
+        print('{}'.format(fizzBuzz), end=' ')
