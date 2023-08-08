22845aded46d7f810dd2a5865ccbad3f56e88c5f
El-gibbor <chiagoziem11@yahoo.com>
2023-08-08 13:22:02 +0400

    fixed length of line to conform with pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,7 +10,8 @@ if number < 0:
 if lastDigit > 5:
     print(f'Last digit of {number:d} is {lastDigit:d} and is greater than 5')
 elif lastDigit < 6 and lastDigit != 0:
-    print(f'Last digit of {number:d} is {lastDigit:d} and is less than 6 and not 0')
+    print(f'Last digit of {number:d} is {lastDigit:d}\
+ and is less than 6 and not 0')
 else:
     print(f'Last digit of {number:d} is {lastDigit:d} and is 0')
 
