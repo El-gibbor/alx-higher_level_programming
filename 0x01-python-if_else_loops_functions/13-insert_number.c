6b1b1cd2a78f0cd7e3ed888313be6ec81c1f0162
El-gibbor <chiagoziem11@yahoo.com>
2023-08-13 00:38:48 +0400

    prints all integers of a list.

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * inserts a number into a sorted singly linked list
+ * @head: address of the head pointer (pointer to a pointer)
+ * @number: number to be inserted
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+}
