f13cc66c0fdbb63157909a288752966ca34315c0
El-gibbor <chiagoziem11@yahoo.com>
2023-08-13 00:41:50 +0400

    prints string in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,12 +6,4 @@ def uppercase(str):
             char -= 32
         print('{}'.format(chr(char)), end='')
     print()
-#!/usr/bin/python3
-""" prints alll intee=gers of a list """
 
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print('{:d}'.format(i))
-    
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
