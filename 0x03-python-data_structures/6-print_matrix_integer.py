e357d1a1ddc1a4d86908ced44928431d4c22d5b1
El-gibbor <chiagoziem11@yahoo.com>
2023-08-13 13:23:15 +0400

    valited input matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) < 1:
+            return None 
     for outer_list in matrix:
         for sub_list in outer_list:
             print('{}'.format(sub_list), end='')
