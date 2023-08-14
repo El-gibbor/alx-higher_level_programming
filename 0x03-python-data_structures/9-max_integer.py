bfd13fbe773f40f9d30d1039fdf337b8859de67c
El-gibbor <chiagoziem11@yahoo.com>
2023-08-14 23:04:12 +0400

    returns the maximum value in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,6 +5,3 @@ def max_integer(my_list=[]):
         return None
     return sorted(my_list)[-1]
   
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
