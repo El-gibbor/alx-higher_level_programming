85f1e16a53e006c80b3f3e2a46cb4611780f66bd
El-gibbor <chiagoziem11@yahoo.com>
2023-08-14 19:56:57 +0400

    gets the max value in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    max_num = my_list[0]
+    for num in my_list:
+        if num > max_num:
+            max_num = num
+    return max_num
