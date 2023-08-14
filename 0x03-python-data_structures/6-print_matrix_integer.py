75886b6aaa5929c0390e8f59964fc452e56ab8c8
El-gibbor <chiagoziem11@yahoo.com>
2023-08-14 05:27:13 +0000

    validated pycodestyle

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) < 1:
-        return None
     for outer_list in matrix:
         for idx, sub_list in enumerate(outer_list):
             if idx < 2:
