44cef487b332dd1ad01dbfd935343a9df497d57a
El-gibbor <chiagoziem11@yahoo.com>
2023-08-16 19:58:18 +0400

    adds up unique numbers in a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    return (sum(set(my_list)))
+
+# ===== for julien & bernard =====
+    # uniq, add_uniq = [], 0
+    # for i in my_list:
+    #     if i not in uniq:
+    #         uniq.append(i)
+    # for j in uniq:
+    #     add_uniq += j
+    # return add_uniq
