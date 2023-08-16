4407aa6d65c465e2c3c3a81d33564eece9c84b03
El-gibbor <chiagoziem11@yahoo.com>
2023-08-16 21:44:45 +0400

    prints a dictionary sorted alphabetically

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    [print(k, v) for k, v in sorted(a_dictionary.items())]  # dict comprehesion
+    for k, v in sorted(a_dictionary.items()):
+        print(k, v)
