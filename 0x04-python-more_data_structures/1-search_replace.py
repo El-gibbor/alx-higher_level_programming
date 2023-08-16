5f783fa222c91199b8725cc1d1e5998ce062a326
El-gibbor <chiagoziem11@yahoo.com>
2023-08-16 18:53:30 +0400

    search and replace elements in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,7 +4,7 @@ def search_replace(my_list, search, replace):
     replaced = [replace if i == search else i for i in my_list]
     return replaced
 
-# ====== a respect to Guido van Rossum =====
+# ====== Lets rep the zen of python =====
 # def search_replace(my_list, search, replace):
 #     replaced = []
 #     for i in my_list:
