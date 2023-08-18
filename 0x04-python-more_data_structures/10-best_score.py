8a1c678b2888f08fc4d741b60532ee52a4b65929
El-gibbor <chiagoziem11@yahoo.com>
2023-08-18 12:24:44 +0400

    returns key of the max int value in a dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary is not None or not a_dictionary:
         return None
     return max(k for k in a_dictionary.keys())
