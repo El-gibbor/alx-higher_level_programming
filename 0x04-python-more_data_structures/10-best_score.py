ee3220ccec59fc9377aab09ddfa4a9a7d9d64b04
El-gibbor <chiagoziem11@yahoo.com>
2023-08-18 07:33:15 +0000

    returns a key with the max int value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,10 @@
 def best_score(a_dictionary):
     if not len(a_dictionary):
         return None
-    return max(k, for k in a_dictionary.keys())
+    return max(k for k in a_dictionary.keys())
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
