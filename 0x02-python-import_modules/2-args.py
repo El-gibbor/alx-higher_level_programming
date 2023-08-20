e61bec6d858a3b89a0f7a76ddd84644c298ff241
El-gibbor <chiagoziem11@yahoo.com>
2023-08-20 16:00:39 +0400

    python cmdline args - prints the number and list of args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from sys import argv
 
     if (len(argv) - 1) == 0:
-        print('0 arguments')
+        print('0 arguments.')
     elif (len(argv) - 1) == 1:
         [print(f'1 argument:\n{i}: {j}') for i, j in enumerate(argv[1:], 1)]
     else:
