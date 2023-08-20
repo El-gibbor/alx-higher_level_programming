e2d6d8c727a87e9f424762c5f7e6c0c7f9558a1a
El-gibbor <chiagoziem11@yahoo.com>
2023-08-20 14:58:17 +0400

    import functions and do a few simple maths

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     a = 10
     b = 5
     print('{:d} + {:d} = {:d}'.format(a, b, calc.add(a, b)))
-    print('{:d} - {:d} = {:d}'.format(a, b, calc.div(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, calc.sub(a, b)))
     print('{:d} * {:d} = {:d}'.format(a, b, calc.mul(a, b)))
     print('{:d} / {:d} = {:d}'.format(a, b, calc.div(a, b)))
