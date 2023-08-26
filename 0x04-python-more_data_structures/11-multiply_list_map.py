acc5527afc579ac453d9583eb34853418a253d37
El-gibbor <chiagoziem11@yahoo.com>
2023-08-26 16:38:08 +0400

    returns a list with all values multiplied by a value. usin map func

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    new = list(map(lambda value: value * number, my_list))
-    return new
+    return list(map(lambda value: value * number, my_list))
