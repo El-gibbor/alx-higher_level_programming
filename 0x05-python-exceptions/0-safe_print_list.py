196781d42e779a4ae39dd24dc24528542e49c9bc
El-gibbor <chiagoziem11@yahoo.com>
2023-08-28 17:04:01 +0400

    prints x elements of a list, handling indexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
 
     for elem in my_list[:x]:
         try:
-            print(my_list[elem], end='')
+            print(elem, end='')
             nb_print += 1
         except IndexError:
             pass
