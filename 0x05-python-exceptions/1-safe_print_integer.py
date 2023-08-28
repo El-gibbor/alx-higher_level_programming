2172d07c83219ff2a9c653ca4203842672c916f3
El-gibbor <chiagoziem11@yahoo.com>
2023-08-28 15:24:15 +0400

    prints an integer with {:d}.format(), handling value & TypeError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        print('{:d}'.format(value), end='\n')
+        print('{:d}'.format(value))
         return True
-    except ValueError:
+    except (TypeError, ValueError):
         return False
