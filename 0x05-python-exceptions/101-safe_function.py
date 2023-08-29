ae1b82f4bbe118159a3acd282774911626241ddf
El-gibbor <chiagoziem11@yahoo.com>
2023-08-29 19:32:23 +0400

    executes a func and prints exception erros to stderr

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+"""executes a func and prints exception erros to stderr"""
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as tb_error:
+        print('Exception:', tb_error, file=sys.stderr)
+        return None
