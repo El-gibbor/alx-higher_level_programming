0a0678418c3eed19a9ce9496d3467f274b84ee0c
El-gibbor <chiagoziem11@yahoo.com>
2023-08-29 15:30:54 +0400

    divides list elem by list2 elem and handle all possible raised exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,6 +2,7 @@
 
 def list_division(my_list_1, my_list_2, list_length=0):
     list_div = []
+
     for i in range(list_length):
         try:
             list_div.append(my_list_1[i] / my_list_2[i])
