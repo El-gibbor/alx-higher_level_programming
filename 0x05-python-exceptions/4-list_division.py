b5a55e2e16616100f557ba88fc11902b84132fac
Chiagoziem El-gibbor <chiagoziem11@yahoo.com>
2023-08-29 16:51:50 +0400

    pending

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
-def divide_elements(a, b):
-    try:
-        return a / b
-    except (ZeroDivisionError):
-        print('division by zero')
-        return 0
-    except (TypeError):
-        print('wrong type')
-        return 0
+# def divide_elements(a, b):
+#     try:
+#         return a / b
+#     except (ZeroDivisionError):
+#         print('division by zero')
+#         return 0
+#     except (TypeError):
+#         print('wrong type')
+#         return 0
 
-def list_division(my_list_1, my_list_2, list_length):
-    return [
-        divide_elements(a, b)
-        for a, b in zip(my_list_1[:list_length], my_list_2[:list_length])
-    ]
+# def list_division(my_list_1, my_list_2, list_length):
+#     return [
+#         divide_elements(a, b)
+#         for a, b in zip(my_list_1[:list_length], my_list_2[:list_length])
+#     ]
