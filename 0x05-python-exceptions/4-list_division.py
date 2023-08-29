6b154285569dfa26a09b35350c034fa9f7634435
El-gibbor <chiagoziem11@yahoo.com>
2023-08-29 16:03:37 +0400

    divides list elem by list2 elem and handle all possible raised exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 
-def list_division(my_list_1, my_list_2, list_length=0):
-    list_div = []
+def divide_elements(a, b):
+    try:
+        return a / b
+    except (ZeroDivisionError):
+        print('division by zero')
+        return 0
+    except (TypeError):
+        print('wrong type')
+        return 0
 
-    for i in range(list_length):
-        try:
-            list_div.append(my_list_1[i] / my_list_2[i])
-        except ZeroDivisionError:
-            print('division by 0')
-            list_div.append(0)
-        except TypeError:
-            print('wrong type')
-            list_div.append(0)
-        except IndexError:
-            print('out of range')
-        finally:
-            pass
-    return list_div
+def list_division(my_list_1, my_list_2, list_length):
+    return [
+        divide_elements(a, b)
+        for a, b in zip(my_list_1[:list_length], my_list_2[:list_length])
+    ]
