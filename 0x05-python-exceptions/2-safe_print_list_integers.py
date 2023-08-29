23d0ebe7bda55c8fb38003a89737cf977e1fed04
El-gibbor <chiagoziem11@yahoo.com>
2023-08-29 07:24:33 +0000

    prints x int elements of a list and  handle a few exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     nb_print = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
             print('{}'.format(my_list[i]), end='')
             nb_print += 1
