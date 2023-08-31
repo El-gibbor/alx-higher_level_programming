1cde53ad3a926d7fef54ec56456e53cf65b546c8
El-gibbor <chiagoziem11@yahoo.com>
2023-08-31 18:43:43 +0400

    implement instance value comparators

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""a class Square that defines a square"""
+
+
+class Square:
+    """class definition"""
+
+    def __init__(self, size=0):
+        """
+            instantiate square with a size attribute
+        Args:
+            size - size of the square
+        raises:
+            ValueError - if size is less than 0
+            TypeError - if value is not an integer
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """retrives private instance attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value=0):
+        """validate and sets attribute value"""
+
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """ Returns the current square area"""
+        return self.__size ** 2
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
