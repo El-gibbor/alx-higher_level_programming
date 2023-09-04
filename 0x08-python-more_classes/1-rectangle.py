31f9b17f80e8ece0b8b40ec56dc1a02dd748ced9
El-gibbor <chiagoziem11@yahoo.com>
2023-09-04 19:28:43 +0400

    instantiate rectangle class with a height attribute

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,8 +18,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """ defines a rectangle """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
