517f3d64e9b67a602148c9d3473ecd827f40fa6e
El-gibbor <chiagoziem11@yahoo.com>
2023-09-04 19:58:14 +0400

    adds a public instance method that returns perimeter and area of the rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -59,4 +59,7 @@ class Rectangle:
 
     def perimeter(self):
         """returns te parimeter of rectangle"""
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
