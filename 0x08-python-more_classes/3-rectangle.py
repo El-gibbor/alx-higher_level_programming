49ca6a523a4081a97a06068e50d270756c631879
El-gibbor <chiagoziem11@yahoo.com>
2023-09-04 20:28:10 +0400

    adds a str method to print object attributes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -67,18 +67,8 @@ class Rectangle:
     def __str__(self) -> str:
         """ returns string representation of objects"""
 
-        # rect = ''
-        # if self.__width == 0 or self.__height == 0:
-        #     return rect
-        # for i in range(self.__height):
-        #     for _ in range(self.__width):
-        #         rect += '#'
-        #     if i < self.__height - 1:
-        #         rect += "\n"
-        # return rect
-
         if self.__width == 0 or self.__height == 0:
             return ""
-        rows = ['#' * self.__width for _ in range(self.__height)]
-        return '\n'.join(rows)
+        hash_row = ['#' * self.__width for _ in range(self.__height)]
+        return '\n'.join(hash_row)
 
