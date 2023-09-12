29ab886b812d7e66e60f27fd36d36ee313c2a84d
El-gibbor <chiagoziem11@yahoo.com>
2023-09-12 13:37:12 +0400

    derived class of BaseGeometry and validate value with the superclass method

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+""" a modeul containing a derived class of BaseGeometry"""
+
+
+class Rectangle(BaseGeometry):
+    """ defines a rectangle, subclass of Geometery """
+
+    def __init__(self, width, height):
+        """ instantiation of instance attributes"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
