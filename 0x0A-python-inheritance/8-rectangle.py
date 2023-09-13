32f1a9aba076a8df2fd6b0a5d75107ffb4c2e2fe
El-gibbor <chiagoziem11@yahoo.com>
2023-09-13 11:31:12 +0400

    fulfilling requirement checks

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """ a module containing a derived class of BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
