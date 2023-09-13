0627e0ec005488ccbfce78da8b90ed99f9c3d8aa
El-gibbor <chiagoziem11@yahoo.com>
2023-09-13 11:25:04 +0400

    fulfilling requirement checks

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,11 +6,13 @@ class BaseGeometry:
     """ defines a class Geometry """
 
     def area(self):
+
         """ raises an exception """
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
         """ validates value """
+
         if type(value) != int:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+"""Derived class of BaseGeometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-""" a modeul containing a derived class of BaseGeometry"""
 
 
 class Rectangle(BaseGeometry):
-    """ defines a rectangle, subclass of Geometery """
+    """defines rectangle using BaseGeometry"""
 
     def __init__(self, width, height):
-        """ instantiation of instance attributes"""
-
+        """Intialize a new Rectangle
+        """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
