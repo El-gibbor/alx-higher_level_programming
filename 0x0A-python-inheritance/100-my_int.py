8f581f96196b5215464337b5f4da790f99463aa6
El-gibbor <chiagoziem11@yahoo.com>
2023-09-13 14:33:11 +0400

    rebel class that overides default behaviour

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# """ A module containing a derived class of int"""
+""" A module containing a derived class of int"""
 
 
 class MyInt(int):
@@ -7,8 +7,8 @@ class MyInt(int):
 
     def __eq__(self, data):
         """Override == opeartor with != """
-        return super().__ne__(data)
+        return self.real != data
 
     def __ne__(self, data):
         """Override != operator with == """
-        return super().__eq__(data)
+        return self.real == data
