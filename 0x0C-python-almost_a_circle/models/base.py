3c4a7eb5cb6dfcbbdf30ad62a5e4db73d129ff63
El-gibbor <chiagoziem11@yahoo.com>
2023-09-16 11:12:38 +0400

    write all serialised objs to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains a base class for all models"""
-from json import dumps
+from json import dumps, dump
+
 
 class Base:
     """ Defines a base class for all models """
@@ -21,8 +22,19 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        
+
         if not list_dictionaries or list_dictionaries is None:
             return '[]'
         else:
             return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+
+        with open(cls.__name__ + '.json', 'w') as j_file:
+            if list_objs is None:
+                j_file.write('[]')
+            else:
+                serialised = [objs.to_dictionary() for objs in list_objs]
+                j_file.write(cls.to_json_string(serialised))
