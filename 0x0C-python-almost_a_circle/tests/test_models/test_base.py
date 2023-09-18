2851903d779742347157dc3a3a41dde432d15324
El-gibbor <chiagoziem11@yahoo.com>
2023-09-18 21:46:56 +0400

    refactored tests and made use of context manager

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,48 +1,13 @@
 #!/usr/bin/python3
 """a Test suit module for the base class"""
 from models.base import Base
-import unittest
+from unittest import TestCase
 
-
-# class Test_base(unittest.TestCase):
-#     """
-#     Tests for the automatic assigning of Ids to all base instance.
-#         - Assigning id automatically.
-#         - Id increases by at new instantiation after the prev one
-#         - Makes use of user provided Id instead of automatic assigning
-#     """
-
-#     def setUp(self):
-#         """ test env setup and instantiation of base objs """
-#         self.base_obj = Base()
-#         self.base_obj_1 = Base()
-#         self.base_obj_2 = Base(99)
-#         self.base_obj_3 = Base("str")
-#         self.base_obj_4 = Base([2, 2, 3])
-
-#     def tearDown(self):
-#         """Test cleanup - delete instance after each test"""
-#         del self.base_obj
-#         del self.base_obj_1
-#         del self.base_obj_2
-#         del self.base_obj_3
-#         del self.base_obj_4
-
-#     def test_base_id(self):
-#         """ Correct Id assignment of all base instance """
-#         self.assertEqual(self.base_obj.id, 1)
-#         self.assertEqual(self.base_obj_1.id, 2)
-#         self.assertEqual(self.base_obj_2.id, 99)
-#         self.assertEqual(self.base_obj_3.id, "str")
-#         self.assertEqual(self.base_obj_4.id, [2, 2, 3])
-
-class TestBase(unittest.TestCase):
-    """
-    Tests for the Base class.
-    """
+class Test_base(TestCase):
+    """ Tests for the automatic assigning of Ids to all base instance."""
 
     def test_auto_increment_id(self):
-        """Test that IDs are assigned automatically and increase."""
+        """Test that IDs are assigned automatically and increases."""
         base_obj_1 = Base()
         base_obj_2 = Base()
         self.assertEqual(base_obj_1.id, 1)
@@ -54,11 +19,12 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base_obj.id, 99)
 
     def test_non_integer_id(self):
-        """Test when a non-integer ID is provided."""
-        base_obj = Base("str")
-        self.assertEqual(base_obj.id, "str")
+        """ Test when a non-integer ID is provided."""
 
-    def test_non_integer_id_list(self):
-        """Test when a non-integer ID (list) is provided."""
-        base_obj = Base([2, 2, 3])
-        self.assertEqual(base_obj.id, [2, 2, 3])
+        with self.subTest():
+            base_obj = Base("str")
+            self.assertEqual(base_obj.id, "str")
+
+        with self.subTest():
+            base_obj = Base([2, 2, 3])
+            self.assertEqual(base_obj.id, [2, 2, 3])
