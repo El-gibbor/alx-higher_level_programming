ce7eee9cfc4164d0b61e18f9cdec3f6dd7d85039
El-gibbor <chiagoziem11@yahoo.com>
2023-09-19 21:30:14 +0400

    returns JSON representation of a str

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" contains a func that converts to json"""
+from json import dumps
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)"""
+
+    return dumps(my_obj)
