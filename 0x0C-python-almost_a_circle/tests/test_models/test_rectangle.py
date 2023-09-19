46adcd8ffb28c914ae90a3e3f83f54ec77071300
El-gibbor <chiagoziem11@yahoo.com>
2023-09-19 20:47:25 +0400

    trying

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 """a Test suit module for the rectangle class"""
-import  unittest
-from models.rectangle import Rectangle
+# import  unittest
+# from models.rectangle import Rectangle
 
 
-class Test_rectangle(unittest.TestCase):
-    """ Tests the functionality of derived class of base """
-    def setUp(self):
-        """ instantiates Rectangle class for testing """
-        self.rect_obj = Rectangle(101, 202)
-        self.rect_obj_2 = Rectangle(10, 22)
+# class Test_rectangle(unittest.TestCase):
+#     """ Tests the functionality of derived class of base """
+#     def setUp(self):
+#         """ instantiates Rectangle class for testing """
+#         self.rect_obj = Rectangle(101, 202)
+#         self.rect_obj_2 = Rectangle(10, 22)
 
-    def tearDown(self):
-        """ deletes instance after each test """
-        del self.rect_obj
-        del self.rect_obj_2
+#     def tearDown(self):
+#         """ deletes instance after each test """
+#         del self.rect_obj
+#         del self.rect_obj_2
 
-    def test_rectangle_id(self):
-        self.assertEqual(self.rect_obj.id, 1)
-        self.assertEqual(self.rect_obj.id, self.rect_obj_2.id - 1)
+#     def test_rectangle_id(self):
+#         self.assertEqual(self.rect_obj.id, 1)
+#         self.assertEqual(self.rect_obj.id, self.rect_obj_2.id - 1)
 
-    def test_x_cordinate(self):
-        self.rect_obj.x = 30
-        self.assertEqual(self.rect_obj.x,30)
+#     def test_x_cordinate(self):
+#         self.rect_obj.x = 30
+#         self.assertEqual(self.rect_obj.x,30)
 
-    def test_y_cordinate(self):
-        self.rect_obj.y = 31
-        self.assertEqual(self.rect_obj.y, 31)
+#     def test_y_cordinate(self):
+#         self.rect_obj.y = 31
+#         self.assertEqual(self.rect_obj.y, 31)
