7de53dfc375d430bb6a01fe92759a39b4345e969
El-gibbor <chiagoziem11@yahoo.com>
2023-09-19 21:19:21 +0400

    appends a string at the end of a text file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" holds a module that appends test to a file"""
+
+
 def append_write(filename="", text=""):
+
     """ appends a string at the end of a text file """
     with open(filename, 'a') as a_file:
         return (a_file.write(text))
