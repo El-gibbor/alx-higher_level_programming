b9ec09bd7c51af95bd104a8be126bd5e9fb7b6fc
El-gibbor <chiagoziem11@yahoo.com>
2023-09-26 13:14:17 +0400

    Students to Json

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Student to Json """
+
+
+class Student:
+    """ a class that defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ initializing public instance attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance.
+        vars() method returns the __dict__ (dictionary mapping) attribute of
+        the given object.
+        """
+        return vars(self)
