03ea980936ee290c9058466d1e4cef72fd0e39d5
El-gibbor <chiagoziem11@yahoo.com>
2023-10-18 00:19:09 +0400

    creates a database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- creates the database hbtn_0c_0 in your MySQL server.
-CREATE DATABASE IF NOT EXIST hbtn_0c_0;
+-- Creates the database hbtn_0c_0 in MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
