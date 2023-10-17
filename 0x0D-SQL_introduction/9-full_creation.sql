0765048cf2f6bb5194b69206b3eff036b461e753
El-gibbor <chiagoziem11@yahoo.com>
2023-10-18 00:59:14 +0400

    creates a new table and insert data into its fields

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- creates a table second_table in the database hbtn_0c_0
--- adds multiple rows
+-- and add/insert multiple rows into it
 CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
