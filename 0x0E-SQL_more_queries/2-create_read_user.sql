bcef3d8e87839e836d74cb928010981330aad28a
El-gibbor <chiagoziem11@yahoo.com>
2023-10-18 20:58:11 +0400

    creates database and user with SELECT privilege

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
---  create the database hbtn_0d_2 and the user user_0d_2
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+-- creates the database hbtn_0d_2 and the user user_0d_2.
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
