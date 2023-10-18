27c5b739593c0ec21c3cdd6b7b64f099b755e76c
El-gibbor <chiagoziem11@yahoo.com>
2023-10-18 23:12:07 +0400

    creates table with unique id, auto incremented and not null

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,5 +5,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
-    name VARCHAR(256)
+    name VARCHAR(256),
+    PRIMARY KEY(id)
 );
