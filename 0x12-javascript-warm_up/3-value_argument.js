409e45eadb1461d13bdeee9ad6cd76fb08d30f4a
El-gibbor <chiagoziem11@yahoo.com>
2023-11-14 00:50:20 +0400

    prints the first arg passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-const msg = (argv[2] === undefined) ? 'No arguments' : argv[2];
+const msg = (argv[2] === undefined) ? 'No argument' : argv[2];
 console.log(msg);
