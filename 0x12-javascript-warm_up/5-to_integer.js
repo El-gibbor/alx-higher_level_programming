522a7fc0b12de5a0ed2f3c057ef26b841714cf0f
El-gibbor <chiagoziem11@yahoo.com>
2023-11-14 01:30:43 +0400

    converts to int and print. prints a given string for NaN

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 const { argv } = require('process');
 
 const toInt = Math.floor(argv[2]);
-isNaN(toInt) ? console.log('Not a number') : console.log(`My number is: ${argv[2]}`);
+isNaN(toInt) ? console.log('Not a number') : console.log(`My number: ${argv[2]}`);
 
