bfbe5e9b847d5fb56c290a9d70da38061308dba3
El-gibbor <chiagoziem11@yahoo.com>
2023-11-14 00:18:28 +0400

    prints msg depending on args passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-const msg = (argv.length < 3) ? 'No argument' : (argv.length === 3) ? 'Argument found' : 'Arguments found';
+const x = argv.length
+const msg = (x < 3) ? 'No argument' : (x === 3) ? 'Argument found' : 'Arguments found';
 console.log(msg);
