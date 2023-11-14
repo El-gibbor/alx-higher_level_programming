8370d16a7411ebf77e81c8a088eb9e3d8bfcf1c3
El-gibbor <chiagoziem11@yahoo.com>
2023-11-14 10:51:02 +0400

    prints a string x times the number of args passed

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+const x = Math.floor(argv[2]);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
