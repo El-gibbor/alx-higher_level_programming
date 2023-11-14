7d792c61cd4f509917bfeb11c7bdc6a91b45c71e
El-gibbor <chiagoziem11@yahoo.com>
2023-11-14 22:36:47 +0400

    finds second biggest value in the argument

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,15 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-const allArgs = argv.slice(2).map(Number);
+const allArgs = argv.slice(2).map(Number); // new array with elements casted to int
 if (allArgs.length <= 1) {
-  console.log(0);
+    console.log(0);
 } else {
-  const secondBig = allArgs.sort((a, b) => a - b).reverse()[1];
-  console.log(secondBig);
+    const secondBig = allArgs.sort((a, b) => a - b).reverse()[1];
+    console.log(secondBig);
 }
+/* sort() takes a comparison function that defines the sort order. The return value is a
+ number that indicate the relative order of the two elements: negative if a is less than b,
+ positive if a is greater than b, and zero if they are equal. without this comparison, the
+ array is sorted as a string which wouldnt be correct when you are expecting a numerical sort.
+*/
