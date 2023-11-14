e8ad4b6c76f3c09e1232463c710524617ca5a769
El-gibbor <chiagoziem11@yahoo.com>
2023-11-14 10:39:31 +0400

    print 3 lines usinf loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arrStr = ['Python is cool', 'C is fun', 'JavaScript is amazing'];
+const arrStr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let j = 0; j < arrStr.length; j++) {
   console.log(arrStr[j]);
