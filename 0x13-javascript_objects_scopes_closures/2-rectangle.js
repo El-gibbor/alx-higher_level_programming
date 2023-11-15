c88cec1077e4ee14d64af44f03c3275ee19fb3cb
El-gibbor <chiagoziem11@yahoo.com>
2023-11-15 19:33:41 +0400

    initializes instance attributes only if they are 0 or positive

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,10 @@
-#!/usr/node
-
+#!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 module.exports = Rectangle;
