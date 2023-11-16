4176f6b6aaffdd979ed64c1de4c07999cde8e185
El-gibbor <chiagoziem11@yahoo.com>
2023-11-16 17:33:07 +0400

    inheritance and handling 'undefined' in arg

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,7 +6,7 @@ class Square extends ParentSquare {
     super(size, size);
   }
 
-  /* 'X' as default arg when no arg is provided (undefinde) */
+  /* 'X' as default arg when no arg is provided (undefined) */
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
