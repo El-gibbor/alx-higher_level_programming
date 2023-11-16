89a55a686b1297bb4959ab4b8482c4655bd4dce9
El-gibbor <chiagoziem11@yahoo.com>
2023-11-16 20:09:18 +0400

    reverse a list without a reverse() method

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-  return list.filter((x) => x === searchElement).length;
+  return list.filter((x) => x === searchElement).length
 };
