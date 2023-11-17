151f7aa86cf1aab7a5ddec1fae7d851147653f26
El-gibbor <chiagoziem11@yahoo.com>
2023-11-17 20:04:42 +0400

    refactored and used with to implement automatic cleanup - db and cusor closing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" a script that lists all states from the database hbtn_0e_0_usa """
+""" Msqldb script that lists all states from the database hbtn_0e_0_usa """
 
 if __name__ == "__main__":
 
