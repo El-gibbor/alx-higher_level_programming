475ad80b62ef6d46020769829619edc2d35ab940
El-gibbor <chiagoziem11@yahoo.com>
2023-11-17 23:38:43 +0400

    lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Msqldb script that lists all states with a name starting with
-N (upper N) from the database hbtn_0e_0_usa
+N (upper N) from the database hbtn_0e_0_usa.
 """
 
 if __name__ == "__main__":
