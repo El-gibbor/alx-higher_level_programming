8b98d4cbd6f7d69b89baa0befe9c4f3b67dfc6e2
El-gibbor <chiagoziem11@yahoo.com>
2023-11-17 23:30:03 +0400

    lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Msqldb script that lists all states with a name starting with N (upper N)
-    from the database hbtn_0e_0_usa """
+""" Msqldb script that lists all states with a name starting with
+    N (upper N) from the database hbtn_0e_0_usa
+"""
 
 if __name__ == "__main__":
 
@@ -8,9 +9,9 @@ if __name__ == "__main__":
     from sys import argv
 
     host, port = "localhost", 3306
-    user_name, password, db_name = argv[1], argv[2], argv[3]
+    user_name, passwd, db_name = argv[1], argv[2], argv[3]
 
-    with MySQLdb.connect(host, user_name, password, db_name, port) as db:
+    with MySQLdb.connect(host, user_name, passwd, db_name, port) as db:
         with db.cursor() as cursor_obj:
             query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
             cursor_obj.execute(query)
