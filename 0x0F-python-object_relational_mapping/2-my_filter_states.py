4a77b31d8dec7ae0eea887b26561eb9d93e178e5
El-gibbor <chiagoziem11@yahoo.com>
2023-11-18 16:47:26 +0400

    list all names matching passed arg in database

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     from sys import argv
 
     h, p = "localhost", 3306,
-    u, p, db_name, search = argv[1], argv[2], argv[3], argv[4]
+    u, ps, db_name, search = argv[1], argv[2], argv[3], argv[4]
 
-    with MySQLdb.connect(host=h, user=u, passwd=p, db=db_name, port=p) as db:
+    with MySQLdb.connect(host=h, user=u, passwd=ps, db=db_name, port=p) as db:
         with db.cursor() as cursor_obj:
             query = """SELECT * FROM states
             WHERE BINARY name = '{}' ORDER BY id ASC""".format(search)
