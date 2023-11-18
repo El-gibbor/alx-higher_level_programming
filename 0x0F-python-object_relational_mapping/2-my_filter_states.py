21ed3a82fcee83d48134caa0de547de46aac281a
El-gibbor <chiagoziem11@yahoo.com>
2023-11-18 16:27:45 +0400

    list all names matching passed arg in database

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,14 +7,13 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    host, port, search = "localhost", 3306,
+    host, port = "localhost", 3306,
     user, passwd, db_name, state_name = argv[1], argv[2], argv[3], argv[4]
 
     with MySQLdb.connect(host, user, passwd, db_name, port) as db_conn:
         with db_conn.cursor() as cursor_obj:
-            query = "SELECT * FROM states WHERE name = '{:s}'\
-            ORDER BY id ASC"
-            cursor_obj.execute(query)
+            query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+            cursor_obj.execute(query, (state_name,))
             fetched_state = cursor_obj.fetchall()
             for state in fetched_state:
                 print(state)
