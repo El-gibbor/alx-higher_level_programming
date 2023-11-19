91409a938554a6a491d78b7a39b940ac410b025a
El-gibbor <chiagoziem11@yahoo.com>
2023-11-19 16:11:43 +0400

    get all states in the state table using Sqlachemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     db_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
 
-    engine = create_engine(db_url, pool_pre_ping=True)
+    engine = create_engine(db_url)
     Base.metadata.create_all(engine)
 
     with Session(engine) as session:
