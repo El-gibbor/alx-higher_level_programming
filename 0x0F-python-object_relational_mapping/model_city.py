2b011944ba17c35b065b3acabf0a996d23ff1753
El-gibbor <chiagoziem11@yahoo.com>
2023-11-21 21:44:05 +0400

    retrieves info about  cities and their associated states

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ This is a python class definition of a city table in the database """
 
-from model_state import Base, State
+from model_state import Base
 from sqlalchemy import Integer, String, Column, ForeignKey
 
 
@@ -13,6 +13,6 @@ class City(Base):
         state_id (int): FK, referencing 'id' column of the 'states' table."""
 
     __tablename__ = "cities"
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer(), unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(ForeignKey(State.id), Integer, nullable=False)
+    state_id = Column(Integer(), ForeignKey('states.id'), nullable=False)
