d60fe376fc5e8e7750021a9b726ce503cf5f1f76
El-gibbor <chiagoziem11@yahoo.com>
2023-11-21 15:13:53 +0400

    removes row from table where column name contains 'a'

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -2,18 +2,19 @@
 """ connects to a MySQL db and removes rows from the 'State' table
 where the letter 'a' is present in the 'name' column."""
 
-from sys import argv
-from sqlalchemy.orm import Session
-from model_state import Base, State
-from sqlalchemy import create_engine
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+    from sqlalchemy import create_engine
 
-conn_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
+    conn_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
 
-engine = create_engine(conn_url)
-Base.metadata.create_all(engine)
+    engine = create_engine(conn_url)
+    Base.metadata.create_all(engine)
 
-with Session(engine) as session:
-    states_with_a = session.query(State).filter(State.name.like("%a%"))
-    for state in states_with_a:
-        session.delete(state)
-    session.commit()
+    with Session(engine) as session:
+        state_with_a = session.query(State).filter(State.name.contains("a"))
+        for state in state_with_a:
+            session.delete(state)
+        session.commit()
