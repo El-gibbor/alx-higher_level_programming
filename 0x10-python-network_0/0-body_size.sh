a3005c587db9430ca3887f55b0c50a8f514a5226
El-gibbor <chiagoziem11@yahoo.com>
2023-12-02 22:53:01 +0400

    displays the size of the body of the response from Url request

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # displays the size of the body of the response from a URL request
-# "man curl" to learn more about the used flags to curl
-
 curl -so /dev/null "$1" -w "%{size_download}\n"
