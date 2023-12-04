50790148ccbf8ccef7f446875db90dbc1345fc21
El-gibbor <chiagoziem11@yahoo.com>
2023-12-05 00:24:42 +0400

    # sends a POST request with parameters to a URL with

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request with parameters to a URL with
-curl -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
