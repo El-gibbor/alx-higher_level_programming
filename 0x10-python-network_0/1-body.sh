d81f7974913e3339408620aa50bd9f5225c31a6d
El-gibbor <chiagoziem11@yahoo.com>
2023-12-04 18:59:58 +0400

    curl and display the body of the response for rewuest with 200 stat code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # curl a URL and displays the body of the response for 200 status code
 curl "$1" -sL
-
-# if [ "$(curl -sL "$1" -w '%{http_code}\n')" == "200" ]; then curl -sL "$1"; fi
