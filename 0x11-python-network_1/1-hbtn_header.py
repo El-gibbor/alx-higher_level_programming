0084728f21362a2cc1b882f2138bc61975317060
El-gibbor <chiagoziem11@yahoo.com>
2023-12-07 00:57:57 +0400

    display variable value in a header of a responds

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """sends a request and display value of the variable in the header responds"""
 
-import requests
-from sys import argv
+if __name__ == "__main__":
+    import requests
+    from sys import argv
 
-req = requests.head("https://alx-intranet.hbtn.io", allow_redirects=True)
-print(req.headers['X-Request-Id'])
+    req = requests.head("https://alx-intranet.hbtn.io", allow_redirects=True)
+    print(req.headers['X-Request-Id'])
