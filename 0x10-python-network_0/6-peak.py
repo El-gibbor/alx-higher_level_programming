6a14d5fbb9d9c483b3c57c852c3603b63162cf07
El-gibbor <chiagoziem11@yahoo.com>
2023-12-08 19:29:30 +0400

    handled case of equality in both sides

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Finds a peak in a list of unsorted integers."""
 
+
 def find_peak(list_of_integers):
     """Recursively finds a peak in a list of unsorted ints."""
     if not list_of_integers:
