369e974556dc92acf98708cb607a20b061a539e1
El-gibbor <chiagoziem11@yahoo.com>
2023-12-08 19:29:01 +0400

    handled case of equality in both sides

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" finds a peak in a list of unsorted integers."""
-
+"""Finds a peak in a list of unsorted integers."""
 
 def find_peak(list_of_integers):
     """Recursively finds a peak in a list of unsorted ints."""
@@ -14,9 +13,14 @@ def find_peak(list_of_integers):
         return max(theList)
 
     mid = len(theList) // 2
+
     if theList[mid] > theList[mid + 1] and theList[mid] > theList[mid - 1]:
         return theList[mid]
     elif theList[mid] < theList[mid - 1]:
         return find_peak(theList[:mid])
+    elif theList[mid] == theList[mid - 1]:  # case of equality in both sides
+        left_peak = find_peak(theList[:mid])
+        right_peak = find_peak(theList[mid:])
+        return max(left_peak, right_peak)
     else:
-        return find_peak(theList[mid + 1:])
+        return find_peak(theList[mid:])
