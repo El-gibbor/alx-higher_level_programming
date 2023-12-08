e2671f5c64a4645e35f8ef663e9732d92e20686e
El-gibbor <chiagoziem11@yahoo.com>
2023-12-08 17:46:33 +0400

    finds a peak in a list of unsorted integers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """Recursively finds a peak in a list of unsorted ints."""
+    theList = list_of_integers
+
+    if len(theList) == 1:
+        return theList[0]
+    elif len(theList) == 2:
+        return max(theList)
+
+    mid = len(theList) // 2
+    if theList[mid] > theList[mid + 1] and theList[mid] > theList[mid - 1]:
+        return theList[mid]
+    elif theList[mid] < theList[mid - 1]:
+        return find_peak(theList[:mid])
+    else:
+        return find_peak(theList[mid + 1:])
