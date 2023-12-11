3885334a7ccef5095e666438839a1fec58180245
El-gibbor <chiagoziem11@yahoo.com>
2023-12-11 20:33:15 +0400

    made it non xcutable when imported

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ manages urllib.error.HTTPError exceptions """
 
-from urllib import request, parse
-from urllib.error import URLError
-from sys import argv
+if __name__ == "__main__":
+    from urllib import request, parse
+    from urllib.error import URLError
+    from sys import argv
 
-try:
-    with request.urlopen(argv[1]) as resp:
-        print(resp.read().decode('utf-8'))
-except URLError as e:
-    print(f'Error code: {e.code}')
+    try:
+        with request.urlopen(argv[1]) as resp:
+            print(resp.read().decode('utf-8'))
+    except URLError as e:
+        print(f'Error code: {e.code}')
