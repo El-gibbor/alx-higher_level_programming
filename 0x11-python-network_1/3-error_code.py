3e64f7891ad949c73414fdb244d1f8abb27be807
El-gibbor <chiagoziem11@yahoo.com>
2023-12-11 20:31:41 +0400

    manages urllib.error.HTTPError exceptions

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+""" manages urllib.error.HTTPError exceptions """
 
+from urllib import request, parse
+from urllib.error import URLError
+from sys import argv
+
+try:
+    with request.urlopen(argv[1]) as resp:
+        print(resp.read().decode('utf-8'))
+except URLError as e:
+    print(f'Error code: {e.code}')
