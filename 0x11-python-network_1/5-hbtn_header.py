c19eb83a59084076447497fb39db982a54445379
El-gibbor <chiagoziem11@yahoo.com>
2023-12-11 22:00:37 +0400

    Response header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""fetches, displays the value of a variable in the response header"""
+"""fetches, displays the value of a X-Request-Id in the response header"""
 
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    print(requests.get(argv[1]).headers['X-Request-Id'])
+    r = requests.get(argv[1])
+    print(r.headers['X-Request-Id'])
