176fcb08a70bae4010568bc11739ac304a91162c
El-gibbor <chiagoziem11@yahoo.com>
2023-12-11 21:45:29 +0400

    added '\t' character for tab space on url response

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
 
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
-    print(f"- type: {type(r.text)}")
-    print(f"- content: {r.text}")
+    print(f"\t- type: {type(r.text)}")
+    print(f"\t- content: {r.text}")
