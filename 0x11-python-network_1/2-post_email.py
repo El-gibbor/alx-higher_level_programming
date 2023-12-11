8ecf8408d87e2db12b8113bd19549c95dbbeedfc
El-gibbor <chiagoziem11@yahoo.com>
2023-12-11 19:30:42 +0400

    sends a POST request to a URL with email as parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,10 +6,10 @@
 from urllib import request, parse
 from sys import argv
 
-email = argv[2]
+email, url = argv[2], argv[1]
 arg_val = {'email': email}
 
 data = parse.urlencode(arg_val).encode('ascii')
-req = request.Request(argv[1], data)
+req = request.Request(url, data)
 with request.urlopen(req) as resp:
     print(resp.read().decode('utf-8'))
