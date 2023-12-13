4504ab7d5c7ef532372f9dad8f8220a5a648f78a
El-gibbor <chiagoziem11@yahoo.com>
2023-12-13 23:58:10 +0400

    done for a reason/usr/bin/python3

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     import requests
 
     r = requests.get('https://alx-intranet.hbtn.io/status')
-    print('Body response:')
-    print(f"\t- type: {type(r.text)}")
-    print(f"\t- content: {r.text}")
+    # print('Body response:')
+    # print(f"\t- type: {type(r.text)}")
+    # print(f"\t- content: {r.text}")
+    # print(dir(r))
+    print(r.__dict__)
