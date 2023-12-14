e4fd1554008b858a0f4f162acec34cb66456f23b
El-gibbor <chiagoziem11@yahoo.com>
2023-12-14 20:40:44 +0400

    basic Api authentication

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Basic Auth with Github API to display user id"""
 
 import requests
+from sys import argv
+from requests import auth
 
+if __name__ == "__main__":
+    r = requests.get('http://api.github.com/user', auth=(argv[1], argv[2]))
+    print(r.json()['id'])
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,6 @@ if __name__ == "__main__":
     import requests
 
     r = requests.get('https://alx-intranet.hbtn.io/status')
-    # print('Body response:')
-    # print(f"\t- type: {type(r.text)}")
-    # print(f"\t- content: {r.text}")
-    # print(dir(r))
-    print(r.__dict__)
+    print('Body response:')
+    print(f"\t- type: {type(r.text)}")
+    print(f"\t- content: {r.text}")
