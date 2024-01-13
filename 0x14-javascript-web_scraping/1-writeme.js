74bf703f70db352b2aeb335805ca9142da363ec7
El-gibbor <chiagoziem11@yahoo.com>
2024-01-13 19:38:56 +0400

    writes a string to a file. string and file are passed via cl

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // writes a string to a file. string and file are passed via cl
 
-const { argv } = require("process")
-const fs = require("fs")
+const { argv } = require('process');
+const fs = require('fs');
 
-fs.writeFile(argv[2], argv[3], "utf-8", (err) => {
-    if (err){
-        console.error(err)
-    }
-})
+fs.writeFile(argv[2], argv[3], 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
