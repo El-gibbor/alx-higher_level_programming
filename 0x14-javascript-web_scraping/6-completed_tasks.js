a528a818a4b4e666e524fd35d4ca6db83fbdb1d0
El-gibbor <chiagoziem11@yahoo.com>
2024-01-16 20:03:17 +0400

    computes the number of tasks completed by user id. from an API

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const r = require('request');
+const uri = 'https://jsonplaceholder.typicode.com/todos';
+
+r.get(uri, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const todos = JSON.parse(body);
+    const tdComplete = {};
+
+    for (const i in todos) {
+      if (todos[i].completed) {
+        // increments countValue of each userId in the 'completed' obj. if
+        // userId does not have an existing count, it initializes the count to 1.
+        tdComplete[todos[i].userId] = (tdComplete[todos[i].userId] + 1 || 1);
+      }
+    }
+    console.log(tdComplete);
+  }
+});
